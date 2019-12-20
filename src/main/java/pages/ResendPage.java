b6696a1d98d6d1f362920e92ac049514792865e6
Kyrkunov <37331950+pashakyrkunov@users.noreply.github.com>
2019-12-20 04:15:15 +0300

    Update ResendPage.java

--- src/main/java/pages/ResendPage.java
+++ src/main/java/pages/ResendPage.java
@@ -16,9 +16,9 @@ public class ResendPage extends BasePage {
     public final By ANS3_2 = By.xpath("//*[@data-code='no']/../button");
     public final By ANS3_3 = By.xpath("//*[@data-code='other']/../button");
     public final By ANS3_3_COMMENT = By.xpath("//*[@data-code='other']/../button//input[@placeholder='Your comment']");
-    public final By SUBMIT_BUTTON = By.xpath(".//*[@class='submit wg-btn wg-btn--navy js-survey-submit']");
-    public final By TWITTER = By.xpath(".//*[@class='wg-footer__social-item']//*[contains(@href,'twitter')]");
-    public final By SHOW_THANKS = By.xpath(".//*[@class='survey-success'][@style='display: block;']");
+    public final By SUBMIT_BUTTON = By.xpath("//*[@class='submit wg-btn wg-btn--navy js-survey-submit']");
+    public final By TWITTER = By.xpath("//*[@class='wg-footer__social-item']//*[contains(@href,'twitter')]");
+    public final By SHOW_THANKS = By.xpath("//*[@class='survey-success'][@style='display: block;']");
 
     public ResendPage(WebDriver driver) {
         super(driver);
