7c568db3c456273996d9db20111f3406b60547e0
Kyrkunov <37331950+pashakyrkunov@users.noreply.github.com>
2019-12-20 04:30:03 +0300

    Update ResendPage.java

--- src/main/java/pages/ResendPage.java
+++ src/main/java/pages/ResendPage.java
@@ -16,7 +16,7 @@ public class ResendPage extends BasePage {
     public final By ANS3_2 = By.xpath("//*[@data-code='no']/../button");
     public final By ANS3_3 = By.xpath("//*[@data-code='other']/../button");
     public final By ANS3_3_COMMENT = By.xpath("//*[@data-code='other']/../button//input[@placeholder='Your comment']");
-    public final By SUBMIT_BUTTON = By.xpath("//*//*[@class='survey-form']//button[@type='submit']");
+    public final By SUBMIT_BUTTON = By.xpath("//*[@class='survey-form']//button[@type='submit']");
     public final By TWITTER = By.xpath("//*[@class='wg-footer__social-item']//*[contains(@href,'twitter')]");
     public final By SHOW_THANKS = By.xpath("//*[@class='survey-success']");
 
