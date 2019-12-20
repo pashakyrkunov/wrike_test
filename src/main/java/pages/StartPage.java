3c2d8050dba526aebde75b9908aaf1d95483dca4
Kyrkunov <37331950+pashakyrkunov@users.noreply.github.com>
2019-12-20 04:40:25 +0300

    Update StartPage.java

--- src/main/java/pages/StartPage.java
+++ src/main/java/pages/StartPage.java
@@ -5,13 +5,11 @@ import org.openqa.selenium.WebDriver;
 
 public class StartPage extends BasePage {
 
-    private static final String HEADER_DESKTOP = ".//*[@class='wg-header__desktop']";
-    private static final String TRIAL_BUTTON = "//*[@class='wg-header__free-trial-button wg-btn wg-btn--green']";
     private static final String TRIAL_LABEL = ".//*[@class='modal-form-trial__label']";
     private static final String EMAIL = "//*[@name='email']";
     private static final String SUBMIT = "//*[@type='submit']";
 
-    public final By GET_STARTED_BUTTON = By.xpath(HEADER_DESKTOP + TRIAL_BUTTON);
+    public final By GET_STARTED_BUTTON = By.xpath("//*[@class='wg-header__desktop']//button[contains(@class,'free-trial-button')]");
     public final By EMAIL_FIELD = By.xpath(TRIAL_LABEL + EMAIL);
     public final By CREATE_ACCOUNT_BUTTON = By.xpath(TRIAL_LABEL + SUBMIT);
 
